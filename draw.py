82920667870358cdd10a26e1d8392366214eb9b4
16underscore <steffen.gminsky@online.de>
2021-11-18 19:50:01 +0100

    first commit

---
+++ draw.py
@@ -0,0 +1,29 @@
+def purple(c):
+	# Colors
+	c.colors.completion.category.bg = 'black'
+	c.colors.completion.even.bg = 'black'
+	c.colors.completion.item.selected.bg = 'indigo'
+	c.colors.completion.item.selected.border.bottom = 'indigo'
+	c.colors.completion.item.selected.border.top = 'indigo'
+	c.colors.completion.odd.bg = 'black'
+	c.colors.completion.match.fg = 'indigo'
+	c.colors.hints.bg = 'black'
+	c.colors.hints.fg = 'purple'
+	c.colors.hints.match.fg = 'indigo'
+	c.colors.statusbar.insert.bg = 'indigo'
+	c.colors.statusbar.private.bg = 'indigo'
+	c.colors.statusbar.url.success.https.fg = 'indigo'
+	c.colors.tabs.indicator.stop = 'indigo'
+	c.colors.tabs.selected.even.bg = '#151515'
+	c.colors.tabs.selected.odd.bg = '#151515'
+	c.colors.tabs.even.bg = 'black'
+	c.colors.tabs.odd.bg = 'black'
+	c.colors.webpage.preferred_color_scheme = 'dark'
+	# Hints
+	c.hints.border = '0px solid'
+	c.hints.radius = 0
+	# Tabs
+	c.tabs.favicons.show = 'never'
+	c.tabs.indicator.padding = {'bottom': 0, 'left': 0, 'right': 0, 'top': 0}
+	c.tabs.indicator.width = 1
+	c.tabs.padding = {'bottom': 4, 'left': 8, 'right': 8, 'top': 4}
